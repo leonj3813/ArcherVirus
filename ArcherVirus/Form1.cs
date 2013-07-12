7cf420bb5214b1d9c543af29d51df78f5b41f805
leonj3813 <ejones3813@gmail.com>
2013-07-11 22:41:17 -0400

    Initial Commit

---
+++ ArcherVirus/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArcherVirus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // As soon as the first form is ready, hide the form. Will not be visible on taskbar or 
+        // in windows task manager applications. Go to Processes to kill it.
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Hide();            
+        }
+
+        // Every timer tick make a new instance of the form Pirate and show it
+        // This is the form that plays the video and sound for the 'virus'         
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Pirate pirate = new Pirate();
+            pirate.Show();
+        }
+    }
+}
---
+++ ArcherVirus/Pirate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace ArcherVirus
+{
+    // This form is setup to open on top, in fullscreen, with no borders.
+    public partial class Pirate : Form
+    {
+        // Make a soundplayer to play the sound for the 'virus'
+        SoundPlayer player = new SoundPlayer();
+        // Make a timer that is the length of the 'virus'
+        System.Timers.Timer timer = new System.Timers.Timer(13000);
+        public Pirate()
+        {
+            InitializeComponent();
+        }
+
+        private void Pirate_Load(object sender, EventArgs e)
+        {
+            try
+            {   
+                //Load the 'virus' gif from an embedded resource
+                System.Reflection.Assembly thisExe;
+                thisExe = System.Reflection.Assembly.GetExecutingAssembly();
+                System.IO.Stream file = thisExe.GetManifestResourceStream("ArcherVirus.Archer.gif");
+
+                //Display the 'virus' gif
+                pictureBox1.Image = Image.FromStream(file);
+                
+                //Load the sound from an embedded resource
+                player.Stream = this.GetType().Assembly.GetManifestResourceStream("ArcherVirus.Archer.wav");
+                player.Play();
+
+                // Make the timer call the method "timer_tick" on each tick.
+                // Enable the timer
+                timer.SynchronizingObject = this;
+                timer.Elapsed += new ElapsedEventHandler(timer_tick); // Everytime timer 
+                timer.Enabled = true;                       // Enable the timer
+            }
+
+            catch { }
+        }
+
+        // This method runs on the timer tick and stops the sound player as well as disposing of the 
+        // form.
+        private void timer_tick(object sender, EventArgs e)
+        {
+            player.Stop();
+            this.Close();
+        }        
+    }
+}
